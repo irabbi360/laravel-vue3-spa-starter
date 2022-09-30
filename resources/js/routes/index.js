2513ed8a27ea6278e03cb916d91bbbd263da1f0c
irabbi360 <fazle08rabbi@gmail.com>
2022-10-01 00:33:09 +0600

    Update index.js

--- resources/js/routes/index.js
+++ resources/js/routes/index.js
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import AuthenticatedLayout from "../layouts/Authenticated";
-import GuestLayout from "../layouts/Guest";
+import AuthenticatedLayout from "../layouts/Authenticated.vue";
+import GuestLayout from "../layouts/Guest.vue";
 
-import PostsIndex from '../components/Posts/Index'
-import PostsCreate from '../components/Posts/Create'
-import PostsEdit from '../components/Posts/Edit'
-import Login from '../components/Login'
+import PostsIndex from '../components/Posts/Index.vue'
+import PostsCreate from '../components/Posts/Create.vue'
+import PostsEdit from '../components/Posts/Edit.vue'
+import Login from '../components/Login.vue'
 
 function auth(to, from, next) {
     if (JSON.parse(localStorage.getItem('loggedIn'))) {
