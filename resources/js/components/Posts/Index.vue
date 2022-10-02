9699d34f584ce69367b9614a6cc8603126dcfecb
irabbi360 <fazle08rabbi@gmail.com>
2022-10-02 22:27:23 +0600

    Update Index.vue

--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -102,28 +102,28 @@
                                 </th>
                             </tr>
                             </thead>
-                            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                            <tbody>
                             <tr v-for="post in posts.data">
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                <td class="px-6 py-4 text-sm">
                                     {{ post.id }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                <td class="px-6 py-4 text-sm">
                                     {{ post.title }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                <td class="px-6 py-4 text-sm">
                                     {{ post.category }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                <td class="px-6 py-4 text-sm">
                                     {{ post.content }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                <td class="px-6 py-4 text-sm">
                                     {{ post.created_at }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                                    <router-link v-if="can('posts.update')"
+                                <td class="px-6 py-4 text-sm">
+                                    <router-link v-if="can('post-edit')"
                                                  :to="{ name: 'posts.edit', params: { id: post.id } }">Edit
                                     </router-link>
-                                    <a href="#" v-if="can('posts.delete')" @click.prevent="deletePost(post.id)"
+                                    <a href="#" v-if="can('post-delete')" @click.prevent="deletePost(post.id)"
                                        class="ml-2">Delete</a>
                                 </td>
                             </tr>
