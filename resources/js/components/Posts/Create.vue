d0b6119b7c7bb7b97db8a208cad50d5906870053
irabbi360 <fazle08rabbi@gmail.com>
2022-10-02 22:21:39 +0600

    create post ui & index updated

--- resources/js/components/Posts/Create.vue
+++ resources/js/components/Posts/Create.vue
@@ -1,70 +1,78 @@
 <template>
-    <form @submit.prevent="submitForm">
-        <!-- Title -->
-        <div>
-            <label for="post-title" class="block font-medium text-sm text-gray-700">
-                Title
-            </label>
-            <input v-model="post.title" id="post-title" type="text" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-            <div class="text-red-600 mt-1">
-                {{ errors.title }}
-            </div>
-            <div class="text-red-600 mt-1">
-                <div v-for="message in validationErrors?.title">
-                    {{ message }}
+    <div class="row justify-content-center my-5">
+        <div class="col-md-6">
+            <div class="card border-0 shadow-sm">
+                <div class="card-body">
+                    <form @submit.prevent="submitForm">
+                        <!-- Title -->
+                        <div class="mb-3">
+                            <label for="post-title" class="form-label">
+                                Title
+                            </label>
+                            <input v-model="post.title" id="post-title" type="text" class="form-control">
+                            <div class="text-red-600 mt-1">
+                                {{ errors.title }}
+                            </div>
+                            <div class="invalid-feedback mt-1">
+                                <div v-for="message in validationErrors?.title">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Content -->
+                        <div class="mb-3">
+                            <label for="post-content" class="form-label">
+                                Content
+                            </label>
+                            <textarea v-model="post.content" id="post-content" class="form-control"></textarea>
+                            <div class="invalid-feedback mt-1">
+                                {{ errors.content }}
+                            </div>
+                            <div class="text-red-600 mt-1">
+                                <div v-for="message in validationErrors?.content">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Category -->
+                        <div class="mb-3">
+                            <label for="post-category" class="form-label">
+                                Category
+                            </label>
+                            <v-select v-model="post.category_id" :options="categories" :reduce="category => category.id" label="text" class="form-control" />
+                            <div class="invalid-feedback mt-1">
+                                {{ errors.category_id }}
+                            </div>
+                            <div class="invalid-feedback mt-1">
+                                <div v-for="message in validationErrors?.category_id">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="thumbnail" class="form-label">
+                                Thumbnail
+                            </label>
+                            <input @change="post.thumbnail = $event.target.files[0]" type="file" class="form-control" id="thumbnail" />
+                            <div class="text-red-600 mt-1">
+                                <div v-for="message in validationErrors?.thumbnail">
+                                    {{ message }}
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Buttons -->
+                        <div class="mt-4">
+                            <button :disabled="isLoading" class="btn btn-primary">
+                                <div v-show="isLoading" class=""></div>
+                                <span v-if="isLoading">Processing...</span>
+                                <span v-else>Save</span>
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
-        <!-- Content -->
-        <div class="mt-4">
-            <label for="post-content" class="block font-medium text-sm text-gray-700">
-                Content
-            </label>
-            <textarea v-model="post.content" id="post-content" class="block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"></textarea>
-            <div class="text-red-600 mt-1">
-                {{ errors.content }}
-            </div>
-            <div class="text-red-600 mt-1">
-                <div v-for="message in validationErrors?.content">
-                    {{ message }}
-                </div>
-            </div>
-        </div>
-        <!-- Category -->
-        <div class="mt-4">
-            <label for="post-category" class="block font-medium text-sm text-gray-700">
-                Category
-            </label>
-            <v-select v-model="post.category_id" :options="categories" :reduce="category => category.id" label="text" class="form-control" />
-            <div class="text-red-600 mt-1">
-                {{ errors.category_id }}
-            </div>
-            <div class="text-red-600 mt-1">
-                <div v-for="message in validationErrors?.category_id">
-                    {{ message }}
-                </div>
-            </div>
-        </div>
-        <div class="mt-4">
-            <label for="thumbnail" class="block font-medium text-sm text-gray-700">
-                Thumbnail
-            </label>
-            <input @change="post.thumbnail = $event.target.files[0]" type="file" id="thumbnail" />
-            <div class="text-red-600 mt-1">
-                <div v-for="message in validationErrors?.thumbnail">
-                    {{ message }}
-                </div>
-            </div>
-        </div>
-        <!-- Buttons -->
-        <div class="mt-4">
-            <button :disabled="isLoading" class="inline-flex items-center px-3 py-2 bg-blue-600 text-white rounded disabled:opacity-75 disabled:cursor-not-allowed">
-                <div v-show="isLoading" class="inline-block animate-spin w-4 h-4 mr-2 border-t-2 border-t-white border-r-2 border-r-white border-b-2 border-b-white border-l-2 border-l-blue-600 rounded-full"></div>
-                <span v-if="isLoading">Processing...</span>
-                <span v-else>Save</span>
-            </button>
-        </div>
-    </form>
+    </div>
 </template>
 <script>
 import { onMounted, reactive } from "vue";
--- resources/js/components/Posts/Index.vue
+++ resources/js/components/Posts/Index.vue
@@ -1,128 +1,151 @@
 <template>
-    <div class="">
-        <div class="min-w-full align-middle">
-            <div class="mb-4 grid lg:grid-cols-4 gap-4">
-                <input v-model="search_global" type="text" placeholder="Search..." class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-            </div>
-            <table class="min-w-full divide-y divide-gray-200 border">
-                <thead>
-                <tr>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <input v-model="search_id" type="text" class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by ID">
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <input v-model="search_title" type="text" class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by Title">
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <v-select v-model="search_category" :options="categories" :reduce="category => category.id" label="text" class="form-control" />
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <input v-model="search_content" type="text" class="inline-block mt-1 w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" placeholder="Filter by Content">
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left"></th>
-                    <th class="px-6 py-3 bg-gray-50 text-left"></th>
-                </tr>
-                <tr>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">ID</span>-->
-                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('id')">
-                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
-                                ID
-                            </div>
-                            <div class="select-none">
+    <div class="row justify-content-center my-5">
+        <div class="col-md-12">
+            <div class="card border-0 shadow-sm">
+                <div class="card-body">
+                    <div class="mb-4">
+                        <input v-model="search_global" type="text" placeholder="Search..."
+                               class="form-control w-25">
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <input v-model="search_id" type="text"
+                                           class="inline-block mt-1 form-control"
+                                           placeholder="Filter by ID">
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <input v-model="search_title" type="text"
+                                           class="inline-block mt-1 form-control"
+                                           placeholder="Filter by Title">
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <v-select v-model="search_category" :options="categories"
+                                              :reduce="category => category.id" label="text" class="form-control w-100"/>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <input v-model="search_content" type="text"
+                                           class="inline-block mt-1 form-control"
+                                           placeholder="Filter by Content">
+                                </th>
+                                <th class="px-6 py-3 text-start"></th>
+                                <th class="px-6 py-3 text-start"></th>
+                            </tr>
+                            <tr>
+                                <th class="px-6 py-3 text-start">
+                                    <div class="d-flex flex-row items-center justify-between cursor-pointer"
+                                         @click="updateOrdering('id')">
+                                        <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                             :class="{ 'font-bold text-blue-600': orderColumn === 'id' }">
+                                            ID
+                                        </div>
+                                        <div class="select-none">
                                 <span :class="{
                                   'text-blue-600': orderDirection === 'asc' && orderColumn === 'id',
                                   'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'id',
                                 }">&uarr;</span>
-                                <span :class="{
+                                            <span :class="{
                                   'text-blue-600': orderDirection === 'desc' && orderColumn === 'id',
                                   'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'id',
                                 }">&darr;</span>
-                            </div>
-                        </div>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</span>-->
-                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('title')">
-                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
-                                Title
-                            </div>
-                            <div class="select-none">
+                                        </div>
+                                    </div>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <div class="flex flex-row items-center justify-between cursor-pointer"
+                                         @click="updateOrdering('title')">
+                                        <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                             :class="{ 'font-bold text-blue-600': orderColumn === 'title' }">
+                                            Title
+                                        </div>
+                                        <div class="select-none">
                                 <span :class="{
                                   'text-blue-600': orderDirection === 'asc' && orderColumn === 'title',
                                   'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'title',
                                 }">&uarr;</span>
-                                <span :class="{
+                                            <span :class="{
                                   'text-blue-600': orderDirection === 'desc' && orderColumn === 'title',
                                   'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'title',
                                 }">&darr;</span>
-                            </div>
-                        </div>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-<!--                        <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Created at</span>-->
-                        <div class="flex flex-row items-center justify-between cursor-pointer" @click="updateOrdering('created_at')">
-                            <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider" :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
-                                Created at
-                            </div>
-                            <div class="select-none">
+                                        </div>
+                                    </div>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Category</span>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <span class="text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Content</span>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    <div class="flex flex-row items-center justify-between cursor-pointer"
+                                         @click="updateOrdering('created_at')">
+                                        <div class="leading-4 font-medium text-gray-500 uppercase tracking-wider"
+                                             :class="{ 'font-bold text-blue-600': orderColumn === 'created_at' }">
+                                            Created at
+                                        </div>
+                                        <div class="select-none">
                                 <span :class="{
                                   'text-blue-600': orderDirection === 'asc' && orderColumn === 'created_at',
                                   'hidden': orderDirection !== '' && orderDirection !== 'asc' && orderColumn === 'created_at',
                                 }">&uarr;</span>
-                                <span :class="{
+                                            <span :class="{
                                   'text-blue-600': orderDirection === 'desc' && orderColumn === 'created_at',
                                   'hidden': orderDirection !== '' && orderDirection !== 'desc' && orderColumn === 'created_at',
                                 }">&darr;</span>
-                            </div>
-                        </div>
-                    </th>
-                    <th class="px-6 py-3 bg-gray-50 text-left">
-                        Actions
-                    </th>
-                </tr>
-                </thead>
-                <tbody class="bg-white divide-y divide-gray-200 divide-solid">
-                <tr v-for="post in posts.data">
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                        {{ post.id }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                        {{ post.title }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                        {{ post.category }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                        {{ post.content }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                        {{ post.created_at }}
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
-                        <router-link v-if="can('posts.update')" :to="{ name: 'posts.edit', params: { id: post.id } }">Edit</router-link>
-                        <a href="#" v-if="can('posts.delete')" @click.prevent="deletePost(post.id)" class="ml-2">Delete</a>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
-
-            <Pagination :data="posts" :limit="3" @pagination-change-page="page => getPosts(page, search_category, search_id, search_title, search_content, search_global, orderColumn, orderDirection)" class="mt-4" />
+                                        </div>
+                                    </div>
+                                </th>
+                                <th class="px-6 py-3 bg-gray-50 text-left">
+                                    Actions
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="bg-white divide-y divide-gray-200 divide-solid">
+                            <tr v-for="post in posts.data">
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    {{ post.id }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    {{ post.title }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    {{ post.category }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    {{ post.content }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    {{ post.created_at }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-no-wrap text-sm leading-5 text-gray-900">
+                                    <router-link v-if="can('posts.update')"
+                                                 :to="{ name: 'posts.edit', params: { id: post.id } }">Edit
+                                    </router-link>
+                                    <a href="#" v-if="can('posts.delete')" @click.prevent="deletePost(post.id)"
+                                       class="ml-2">Delete</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <Pagination :data="posts" :limit="3"
+                                @pagination-change-page="page => getPosts(page, search_category, search_id, search_title, search_content, search_global, orderColumn, orderDirection)"
+                                class="mt-4"/>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { ref, onMounted, watch } from "vue";
+import {ref, onMounted, watch} from "vue";
 import usePosts from "../../composables/posts";
 import useCategories from "../../composables/categories";
-import { useAbility } from '@casl/vue'
+import {useAbility} from '@casl/vue'
 
 export default {
     setup() {
@@ -133,9 +156,9 @@ export default {
         const search_global = ref('')
         const orderColumn = ref('created_at')
         const orderDirection = ref('desc')
-        const { posts, getPosts, deletePost } = usePosts()
-        const { categories, getCategories } = useCategories()
-        const { can } = useAbility()
+        const {posts, getPosts, deletePost} = usePosts()
+        const {categories, getCategories} = useCategories()
+        const {can} = useAbility()
         onMounted(() => {
             getPosts()
             getCategories()
