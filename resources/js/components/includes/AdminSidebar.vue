5767441dcc46e5e9a31360f0ac26bf3b29a8b422
irabbi360 <fazle08rabbi@gmail.com>
2022-12-10 23:18:32 +0600

    admin sidebar updated

--- resources/js/components/includes/AdminSidebar.vue
+++ resources/js/components/includes/AdminSidebar.vue
@@ -3,13 +3,18 @@
         <div class="pt-3 sidebar-sticky">
             <ul id="menu" class="nav flex-column mb-2">
                 <li class="nav-item">
-                    <a href="#" class="nav-link">
+                    <router-link to="/admin" class="nav-link">
                         <svg width="22" height="22" viewBox="0 0 22 22">
                             <path
                                 d="M17.4167 4.58333V6.41667H13.75V4.58333H17.4167ZM8.25 4.58333V10.0833H4.58333V4.58333H8.25ZM17.4167 11.9167V17.4167H13.75V11.9167H17.4167ZM8.25 15.5833V17.4167H4.58333V15.5833H8.25ZM19.25 2.75H11.9167V8.25H19.25V2.75ZM10.0833 2.75H2.75V11.9167H10.0833V2.75ZM19.25 10.0833H11.9167V19.25H19.25V10.0833ZM10.0833 13.75H2.75V19.25H10.0833V13.75Z"></path>
                         </svg>
                         Dashboard
-                    </a>
+                    </router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link :to="{ name: 'posts.index' }" class="nav-link">
+                        Posts
+                    </router-link>
                 </li>
                 <li class="nav-item">
                     <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
@@ -51,7 +56,7 @@
 
 <script>
 export default {
-    // name: "AdminSidebar"
+    name: "AdminSidebar"
 }
 </script>
 
