b38c7a4d01ce92ae06fee658393c5c60326fa1f8
Fazle Rabbi <fazle08rabbi@gmail.com>
2022-12-14 12:02:58 +0600

    role permission controller added

---
+++ app/Http/Controllers/Api/RoleController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreRoleRequest;
+use App\Http\Resources\RoleResource;
+use App\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $orderColumn = request('order_column', 'created_at');
+        if (!in_array($orderColumn, ['id', 'name', 'created_at'])) {
+            $orderColumn = 'created_at';
+        }
+        $orderDirection = request('order_direction', 'desc');
+        if (!in_array($orderDirection, ['asc', 'desc'])) {
+            $orderDirection = 'desc';
+        }
+        $roles = Role::
+            when(request('search_id'), function ($query) {
+                $query->where('id', request('search_id'));
+            })
+            ->when(request('search_title'), function ($query) {
+                $query->where('name', 'like', '%'.request('search_title').'%');
+            })
+            ->when(request('search_global'), function ($query) {
+                $query->where(function($q) {
+                    $q->where('id', request('search_global'))
+                        ->orWhere('name', 'like', '%'.request('search_global').'%');
+
+                });
+            })
+            ->orderBy($orderColumn, $orderDirection)
+            ->paginate(50);
+
+        return RoleResource::collection($roles);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreRoleRequest $request)
+    {
+        $this->authorize('category-create');
+        $role = Role::create($request->validated());
+
+        return new RoleResource($role);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
