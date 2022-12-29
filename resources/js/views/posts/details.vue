6489d3d31bed8c68747e380aecdeff3565f5adda
irabbi360 <fazle08rabbi@gmail.com>
2022-12-29 15:02:20 +0600

    post details updated

--- resources/js/views/posts/details.vue
+++ resources/js/views/posts/details.vue
@@ -35,7 +35,7 @@
                             </li>
                         </ol>
                     </div>
-                    <div class="p-4">
+                    <!-- <div class="p-4">
                         <h4 class="fst-italic">Archives</h4>
                         <ol class="list-unstyled mb-0">
                             <li><a href="#">March 2021</a></li>
@@ -51,7 +51,7 @@
                             <li><a href="#">May 2020</a></li>
                             <li><a href="#">April 2020</a></li>
                         </ol>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
