29ac31332def53a31d9796bb7560e0f891d530db
irabbi360 <fazle08rabbi@gmail.com>
2022-12-29 12:19:48 +0600

    Home & admin sidebar updated

--- resources/js/components/includes/AdminSidebar.vue
+++ resources/js/components/includes/AdminSidebar.vue
@@ -61,24 +61,6 @@
                         <span class="d-none d-sm-inline ps-2">Categories</span>
                     </router-link>
                 </li>
-                <li class="nav-item">
-                    <a href="#submenu3" data-bs-toggle="collapse" class="nav-link">
-                        <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
-                    <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                        <li class="w-100">
-                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                        </li>
-                        <li>
-                            <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                        </li>
-                    </ul>
-                </li>
             </ul>
         </div>
     </nav>
--- resources/js/views/home/index.vue
+++ resources/js/views/home/index.vue
@@ -2,7 +2,7 @@
     <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:text-gray-600 dark:text-gray-400 text-sm">
-                <h1>Welcome to laravel 9 Vue.js 3 Stater</h1>
+                <h1>Welcome to Laravel 9 Vue.js 3 Stater</h1>
             </div>
         </div>
     </div>
