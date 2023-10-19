7f7100ae739924f3548b1f6cbdd0ad6bf7579703
Fazle Rabbi <35403788+irabbi360@users.noreply.github.com>
2023-10-19 09:31:45 +0600

    blog post create ui update

--- resources/js/views/admin/posts/Create.vue
+++ resources/js/views/admin/posts/Create.vue
@@ -1,9 +1,10 @@
 <template>
-    <div class="row my-5">
-        <div class="col-md-8">
-            <div class="card border-0 shadow-sm">
-                <div class="card-body">
-                    <form @submit.prevent="submitForm">
+    <form @submit.prevent="submitForm">
+        <div class="row my-5">
+            <div class="col-md-8">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body">
+
                         <!-- Title -->
                         <div class="mb-3">
                             <label for="post-title" class="form-label">
@@ -24,7 +25,7 @@
                             <label for="post-content" class="form-label">
                                 Content
                             </label>
-                            <TextEditorComponent v-model="post.content" />
+                            <TextEditorComponent v-model="post.content"/>
                             <div class="text-danger mt-1">
                                 {{ errors.content }}
                             </div>
@@ -34,12 +35,38 @@
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="card border-0 shadow-sm">
+                    <div class="card-body">
+                        <h6>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                            </svg> Action
+                        </h6>
+                        <div class="mt-3 text-center">
+                            <button :disabled="isLoading" class="btn btn btn-outline-primary me-2">
+                                <div v-show="isLoading" class=""></div>
+                                <span v-if="isLoading">Processing...</span>
+                                <span v-else>Save Draft</span>
+                            </button>
+                            <button :disabled="isLoading" class="btn btn-primary">
+                                <div v-show="isLoading" class=""></div>
+                                <span v-if="isLoading">Processing...</span>
+                                <span v-else>Publish</span>
+                            </button>
+                        </div>
+                        <h6 class="mt-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                            </svg> Category
+                        </h6>
                         <!-- Category -->
                         <div class="mb-3">
-                            <label for="post-category" class="form-label">
-                                Category
-                            </label>
-                            <v-select multiple v-model="post.categories" :options="categoryList" :reduce="category => category.id" label="name" class="form-control" />
+                            <v-select multiple v-model="post.categories" :options="categoryList"
+                                      :reduce="category => category.id" label="name" class="form-control" placeholder="Select category"/>
                             <div class="text-danger mt-1">
                                 {{ errors.categories }}
                             </div>
@@ -50,67 +77,64 @@
                             </div>
                         </div>
                         <div class="mb-3">
-                            <label for="thumbnail" class="form-label">
-                                Thumbnail
-                            </label>
-                            <input @change="post.thumbnail = $event.target.files[0]" type="file" class="form-control" id="thumbnail" />
+                            <h6 class="mt-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-down-square" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M15 2a1 1 0 0 0-1-1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2zM0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.5 2.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
+                                </svg> Thumbnail
+                            </h6>
+                            <input @change="post.thumbnail = $event.target.files[0]" type="file" class="form-control"
+                                   id="thumbnail"/>
                             <div class="text-danger mt-1">
                                 <div v-for="message in validationErrors?.thumbnail">
                                     {{ message }}
                                 </div>
                             </div>
                         </div>
-                        <!-- Buttons -->
-                        <div class="mt-4">
-                            <button :disabled="isLoading" class="btn btn-primary">
-                                <div v-show="isLoading" class=""></div>
-                                <span v-if="isLoading">Processing...</span>
-                                <span v-else>Save</span>
-                            </button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-
-        </div>
-    </div>
+    </form>
 </template>
 <script setup>
-    import { onMounted, reactive } from "vue";
-    import TextEditorComponent from "@/components/TextEditorComponent.vue";
-    import useCategories from "@/composables/categories";
-    import usePosts from "@/composables/posts";
-    import { useForm, useField, defineRule } from "vee-validate";
-    import { required, min } from "@/validation/rules"
-    defineRule('required', required)
-    defineRule('min', min);
+import {onMounted, reactive} from "vue";
+import TextEditorComponent from "@/components/TextEditorComponent.vue";
+import useCategories from "@/composables/categories";
+import usePosts from "@/composables/posts";
+import {useForm, useField, defineRule} from "vee-validate";
+import {required, min} from "@/validation/rules"
 
-    // Define a validation schema
-    const schema = {
-        title: 'required|min:5',
-        content: 'required|min:50',
-        categories: 'required'
-    }
-    // Create a form context with the validation schema
-    const { validate, errors } = useForm({ validationSchema: schema })
-    // Define actual fields for validation
-    const { value: title } = useField('title', null, { initialValue: '' });
-    const { value: content } = useField('content', null, { initialValue: '' });
-    const { value: categories } = useField('categories', null, { initialValue: '', label: 'category' });
-    const { categoryList, getCategoryList } = useCategories()
-    const { storePost, validationErrors, isLoading } = usePosts()
-    const post = reactive({
-        title,
-        content,
-        categories,
-        thumbnail: ''
+defineRule('required', required)
+defineRule('min', min);
+
+// Define a validation schema
+const schema = {
+    title: 'required|min:5',
+    content: 'required|min:50',
+    categories: 'required'
+}
+// Create a form context with the validation schema
+const {validate, errors} = useForm({validationSchema: schema})
+// Define actual fields for validation
+const {value: title} = useField('title', null, {initialValue: ''});
+const {value: content} = useField('content', null, {initialValue: ''});
+const {value: categories} = useField('categories', null, {initialValue: '', label: 'category'});
+const {categoryList, getCategoryList} = useCategories()
+const {storePost, validationErrors, isLoading} = usePosts()
+const post = reactive({
+    title,
+    content,
+    categories,
+    thumbnail: ''
+})
+
+function submitForm() {
+    validate().then(form => {
+        if (form.valid) storePost(post)
     })
-    function submitForm() {
-        validate().then(form => { if (form.valid) storePost(post) })
-    }
-    onMounted(() => {
-        getCategoryList()
-    })
+}
+
+onMounted(() => {
+    getCategoryList()
+})
 </script>
