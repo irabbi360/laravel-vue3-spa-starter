682061c994b6cbdc08e62e65a141b2f0f3baf6a9
Fazle Rabbi <fazle08rabbi@gmail.com>
2025-01-24 10:23:14 +0600

    move to composable

--- resources/js/views/admin/activity-log/Index.vue
+++ resources/js/views/admin/activity-log/Index.vue
@@ -144,65 +144,13 @@
 </template>
 
 <script setup>
-import axios from 'axios';
-import {ref, onMounted, watch, computed} from 'vue'
-import {useRoute, useRouter} from "vue-router";
+import {watch} from 'vue'
+import {useRoute} from "vue-router";
+import useActivityLogs from '@/composables/activityLogs.js'
+
+const {activities, getActivityLogs, activeFilter, searchTerm, currentPage, rangeStart, rangeEnd, totalPages, total, updateFilter, applyFilters, changePage} = useActivityLogs()
 
 const route = useRoute();
-const router = useRouter();
-const activities = ref();
-const activeFilter = ref(route.query.filter || 'all');
-const searchTerm = ref(route.query.search || '');
-
-// Pagination
-const currentPage = ref(parseInt(route.query.page) || 1); // Current page
-const perPage = ref(15); // Items per page
-const total = ref(0); // Total number of items
-
-// Pagination Helpers
-const totalPages = computed(() => Math.ceil(total.value / perPage.value));
-const rangeStart = computed(() => (currentPage.value - 1) * perPage.value + 1);
-const rangeEnd = computed(() =>
-    Math.min(currentPage.value * perPage.value, total.value)
-);
-
-const loadActivityLogs = (() => {
-    const params = {
-        filter: activeFilter.value !== 'all' ? activeFilter.value : undefined,
-        search: searchTerm.value || undefined,
-        page: currentPage.value,
-        per_page: perPage.value,
-    };
-
-    axios.get(`/api/activity-logs/`, { params })
-        .then(({data}) => {
-            activities.value = data;
-            total.value = data.meta.total
-        })
-});
-
-// Update query parameters and refetch data when page changes
-const changePage = (page) => {
-    if (page >= 1 && page <= totalPages.value) {
-        currentPage.value = page;
-        applyFilters();
-    }
-};
-
-const updateFilter = (filter) => {
-    activeFilter.value = filter;
-    applyFilters();
-};
-
-const applyFilters = () => {
-    router.push({
-        query: {
-            filter: activeFilter.value !== 'all' ? activeFilter.value : undefined,
-            search: searchTerm.value || undefined,
-            page: currentPage.value,
-        },
-    });
-};
 
 watch(
     () => route.query,
@@ -210,7 +158,7 @@ watch(
         activeFilter.value = newQuery?.filter || 'all';
         searchTerm.value = newQuery?.search || '';
         currentPage.value = parseInt(newQuery.page) || 1;
-        loadActivityLogs(activeFilter.value)
+        getActivityLogs()
     },
     {
         deep: true,
@@ -218,7 +166,4 @@ watch(
     }
 );
 
-onMounted(() => {
-    loadActivityLogs()
-})
 </script>
